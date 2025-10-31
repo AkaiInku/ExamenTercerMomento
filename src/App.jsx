71f3d986dff8687514127c7bf17cb908c33dc3e1
Damian Quintero Sierra <131413896+AkaiInku@users.noreply.github.com>
2025-10-31 12:06:38 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Registro from './pages/Registro/Registro'
+import Dashboard from './pages/Dashboard/Dashboard'
 import './styles/App.css'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   )
